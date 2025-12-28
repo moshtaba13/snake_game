c14cfa13a1901df6f4cac51830baa244ab099c88
niki <nikitaadelnia@gmail.com>
2025-12-28 23:17:19 +0330

    add apple in map

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "map.h"
 #include "menu.h"
 
 int main()
 {
+    srand(time(NULL));
     printf("welcome to snake game!\n");
 
     enter();
     menu_game();
+
+    place_apple();
+    draw_maps();
 
     return 0;
 }
--- map.h
+++ map.h
@@ -5,6 +5,8 @@
 void map1(void);
 void map2(void);
 
+void place_apple(void);
 void draw_maps(void);
+
 
 #endif
