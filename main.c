884d63a3ba9e45cadc844548db8807193ba34efc
niki <nikitaadelnia@gmail.com>
2026-01-01 20:09:27 +0330

    set loop game

--- main.c
+++ main.c
@@ -2,20 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 #include "map.h"
 #include "menu.h"
+#include "snake.h"
 
 int main()
 {
     srand(time(NULL));
+    system("cls");
     printf("welcome to snake game!\n");
 
     enter();
     menu_game();
-
+    snake_movment();
+    which_apple();
     place_apple();
-    draw_maps();
+
+   while (1)
+    {
+        keyboard();
+        snake_movment();
+        reset();
+        draw_maps();
+        Sleep(150);
+
+    }
 
     return 0;
 }
