6aa15976bf3077f0a0f8463609c60868d134af8b
niki <nikitaadelnia@gmail.com>
2026-01-01 16:59:49 +0330

    set snake on map

---
+++ snake.c
@@ -0,0 +1,103 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+#include "snake.h"
+#include "map.h"
+
+#define MAX 100
+
+
+int x = WIDTH / 2, 
+y = HEIGHT / 2;
+
+int length = 1, key = 3, score = 0;
+
+
+int snakex[MAX], snakey[MAX];
+
+void keyboard() {
+    if (kbhit()) {
+        switch (getch())
+        {
+        case 'w':
+            y++;
+            key = 1;
+            break;
+
+        case 's':
+            y--;
+            key = 2;
+            break;
+        
+        case 'd':
+            x++;
+            key = 3;
+            break;
+        
+        case 'a':
+            x--;
+            key = 4;
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    else {
+        switch (key)
+        {
+        case 1:
+            y++;
+            break;
+ 
+        case 2:
+            y--;
+            break;
+        
+        case 3:
+            x++;
+            break;
+        
+        case 4:
+            x--;
+            break;
+               
+        default:
+            break;
+        }
+    }
+}
+
+void snake_movment() {
+    snakex[0] = x;
+    snakey[0] = y;
+
+    if (x == applex && y == appley) {
+        if (apple == 1) 
+            score += 3;
+        
+        else {
+            score++;
+            length++;
+        }
+        
+        which_apple();
+        place_apple();
+    }
+
+    for (int i = 1; i < length; i++) {
+        snakex[i] = snakex[i + 1];
+        snakey[i] = snakey[i + 1];
+    }
+}
+
+int draw_snake(int i, int j) {
+    for(int k = 0; k < length; k++) {
+        if (snakex[k] == j && snakey[k] == i) {
+            return 1;
+        }
+    }
+    return 0;
+}
---
+++ snake.h
@@ -0,0 +1,12 @@
+
+#ifndef SNAKE_H
+#define SNAKE_H
+
+void keyboard(void);
+void snake_movment(void);
+void snake_movment(void); 
+int draw_snake(int ,int );
+
+extern int length;
+
+#endif
